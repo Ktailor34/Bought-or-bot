7a3ce8629ef3dfb3fb1c21a9e90e456ac7caa77d
Noah Sewell <noh@Noahs-MacBook-Pro.local>
2020-11-18 12:21:16 -0500

    Added basic structure code. Erroring out on some of the featres. Just need to remove them and try again @Shivam

--- knn.py
+++ knn.py
@@ -1,1 +1,29 @@
 #implenting knn algorithm
+
+#datastucture libraries
+import  numpy as np
+import pandas as pd
+import json
+
+#modeling
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import neighbors, datasets
+
+n_neighbors = 15
+
+with open('yelp_combined_dataset.json') as f:
+	data = json.load(f)
+
+X = pd.DataFrame.from_dict(data)
+Y = X['isFake']
+
+X.drop(['isFake'], axis=1)
+
+#X = features
+#Y = target
+
+neigh = KNeighborsClassifier(n_neighbors)
+neigh.fit(X,Y)
+
+
