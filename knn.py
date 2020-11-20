7f1042d5acf047c30805618845795f63273fa2ad
Shivam Patel <spatel95@vols.utk.edu>
2020-11-19 19:45:40 -0500

    knn algorithm and graphs

--- knn.py
+++ knn.py
@@ -1,6 +1,7 @@
 #implenting knn algorithm
 
 #datastucture libraries
+from sklearn.model_selection import train_test_split
 import  numpy as np
 import pandas as pd
 import json
@@ -10,20 +11,51 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import neighbors, datasets
 
-n_neighbors = 15
+#analysis
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-with open('yelp_combined_dataset.json') as f:
-	data = json.load(f)
+yelp_data = pd.read_json('yelp_combined_dataset.json')
+bot_data = pd.read_csv('bot_reviews.csv')
 
-X = pd.DataFrame.from_dict(data)
-Y = X['isFake']
+#drop not needed columns
+yelp_data= yelp_data.drop(columns = ["user_id", "text", "friends"])
+bot_data =bot_data.drop(columns = ["rowCount", "text"])
 
-X.drop(['isFake'], axis=1)
+#combine data
+combined_data = yelp_data.append(bot_data)
 
-#X = features
-#Y = target
 
-neigh = KNeighborsClassifier(n_neighbors)
-neigh.fit(X,Y)
+#change feature values    
+change_to_int = {'N':-1, 'Y':1}
+combined_data['isFake'] = combined_data['isFake'].replace(change_to_int)
+change_to_int = {'pos':1, 'neu':0, 'neg':-1}
+combined_data['reviewType'] = combined_data['reviewType'].replace(change_to_int)
+
+
+#separate features
+X= combined_data[['stars', 'review_count', 'reviewType', 'friendCount']]  #features
+Y= combined_data[['isFake']] #target
+
+#train test split (with randomization)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = .30, random_state= 5)
+
+#perform knn
+
+clf = neighbors.KNeighborsClassifier(n_neighbors=5, p=2, metric='euclidean')
+clf.fit(x_train, y_train.values.ravel())
+
+
+#metric report
+y_expect = y_test
+y_pred = clf.predict(x_test)
+print(classification_report(y_expect,y_pred))
+
+#confusion matrix
+print(confusion_matrix(y_expect, y_pred))
+
+
+
+
 
 
